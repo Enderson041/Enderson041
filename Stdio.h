daed6e1f2b09cf0747900230be8ea5d891250f5e
Enderson041 <158104556+Enderson041@users.noreply.github.com>
2024-01-29 20:43:15 -0300

    Create Stdio.h

---
+++ Stdio.h
@@ -0,0 +1,14 @@
+Include: <Stdio.h>
+Include: <cs50.h>
+int main (void)
+{
+   string answer = get_string ("Qual é seu nome?");
+printf ( "olá, %s", resposta);
+}
+        
+
+
+<!---
+Enderson041/Enderson041 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
